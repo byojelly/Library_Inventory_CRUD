3505e1a09c852067e164a96388839afee9d48aef
Alex <github email address>
2018-01-25 12:15:16 -0500

    add section to book edit show page

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -9,7 +9,7 @@ class BookController < HelperController
     get '/books/:id' do
 
         @book = Book.find_by(id: params[:id])
-#binding.pry
+binding.pry
         erb :'/books/show'
     end
     get '/books/:id/edit' do
@@ -31,6 +31,11 @@ binding.pry
           if   params[:name]=="" || params[:author]=="" || params[:pages]=="" || params[:available]==""
                   flash[:message] = "Please do not leave the input sections empty when submiting an edit."
                   redirect "/books/#{params[:id]}/edit"
+          elsif !is_number?(params[:pages])
+                  flash[:message] = "Please ensure that the pages input is numerical."
+                  redirect "/books/#{params[:id]}/edit"
+    #      elsif 
+
           else
                   @book = Book.find_by(id: params[:id])
                   @book.update( name: params[:name],
