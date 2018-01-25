2a430664bf63fea6592a1a639cc46ccc9aca1c78
Alex <github email address>
2018-01-24 20:48:17 -0500

    add new library

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -9,50 +9,67 @@ class LibraryController < HelperController
         redirect '/'
     end
     get '/libraries/new' do
-binding.pry
           if librarian_logged_in?
               erb :'/libraries/new'
           else
             redirect '/login'
           end
     end
+#not putting a lot of validations to save time
+    post  '/libraries/new' do
+binding.pry
+          if params[:name]=="" || params[:contact_phone]=="" || params[:contact_email]=="" || params[:address_street]=="" || params[:address_city]=="" || params[:address_state]=="" || params[:address_zipcode]=="" || params[:hours_of_operation]==""
+                  flash[:message] = "Please do not leave input fields empty."
+                  redirect '/libraries/new'
+          else
+              @library = Library.create(name: params[:name], contact_phone: params[:contact_phone], contact_email: params[:contact_email], address_street: params[:address_street], address_city: params[:address_city], address_state: params[:address_state], address_zipcode: params[:address_zipcode], hours_of_operation: params[:hours_of_operation])
 
-#    post '/librarians/new' do
-#
-#    end
-#    @librarian = Librarian.find_by(id: session[:librarian_id])
-##binding.pry
-#      if params.has_key?("library_id")
-#            if params[:name]=="" || params[:age]=="" || params[:start_year]==""
-#                    flash[:message] = "Please do not leave name/age/first year empty during onboarding."
-#                    erb :'/librarians/onboarding'
-#            elsif !is_number?(params[:age])
-#                    flash[:message] = "Please make sure that your age is numerical."
-#                    erb :'/librarians/onboarding'
-#
-#            elsif !is_number?(params[:start_year])
-#                            flash[:message] = "Please make sure that your first year worked input is numerical."
-#                            erb :'/librarians/onboarding'
-#            else
-#              @librarian.name = params[:name]
-#
-#              @librarian.age = params[:age].to_i
-#              @librarian.start_year = params[:start_year].to_i
-#              @librarian.library_id = params[:library_id].to_i
-#              @library = Library.find_by(id: @librarian.library_id)
-#
-#              @library.librarians << @librarian
-#              @librarian.save
-#              redirect "/librarians/#{@librarian.id}"
-#            end
-#      else
-#      flash[:message] = "Please try again. You must select a local library during the onboarding."
-#        erb :'/librarians/onboarding'
-#      end
-#
-#
-#
-#
+              redirect redirect "/libraries/#{@library.id}"
+          end
+    end
+
+# =>                    post '/signup' do
+# =>                #binding.pry
+# =>                        #lets do some signup validations
+# =>                                    #make sure, (only 1 user is selected), and that (both buttons arent left blank)
+# =>                                  if (params.has_key?("consumer") && params.has_key?("librarian")) || (!params.has_key?("consumer") && !params.has_key?("librarian"))
+# =>                                    flash[:message] = "Please select either Consumer or Librarian as a usertype. Not both."
+# =>                                      redirect '/signup'
+# =>                                    #make sure username, email and password arent blank
+# =>                                  elsif params[:username]=="" || params[:email]=="" || params[:password]==""
+# =>                                    flash[:message] = "Please do not leave username/email/password empty."
+# =>                                      redirect '/signup'
+# =>                                    #if the consumer was checked
+# =>                                  elsif params.has_key?("consumer")
+# =>                                        #error if the user already exists
+# =>                                        if !!Consumer.find_by(username: params[:username])
+# =>                                                flash[:message] = "This Consumer username has already been taken. Please makeup a new username."
+# =>                                                redirect to '/signup'
+# =>                                        #if user doesnt exist create Consumer
+# =>                                        else
+# =>                                            @consumer = Consumer.create(username: params[:username], email: params[:email], password: params[:password])
+# =>                                            session[:consumer_id] = @consumer.id
+# =>                #binding.pry
+# =>                                            erb :'/consumers/onboarding'
+# =>                                        end
+# =>                                    #if librarian was checked
+# =>                                  elsif params.has_key?("librarian")
+# =>                                          #see if username exists
+# =>                                        if !!Librarian.find_by(username: params[:username])
+# =>                                                flash[:message] = "This Librarian username has already been taken. Please makeup a new username."
+# =>                                                redirect to '/signup'
+# =>                                        else
+# =>                                          #if username doesnt exist create Librarian
+# =>                                            @librarian = Librarian.create(username: params[:username], email: params[:email], password: params[:password])
+# =>                                            session[:librarian_id] = @librarian.id
+# =>                #binding.pry
+# =>                                            erb :'/librarians/onboarding'
+# =>                                        end
+# =>
+# =>                                  end
+# =>
+# =>
+# =>                     end
 
 
 
