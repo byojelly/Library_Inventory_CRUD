2a7b5b3de81bcd0df3695df2dfa0342c0b8ef18e
Alex <github email address>
2018-02-06 23:29:41 -0500

    sections new and edit mass assignment

--- app/controllers/section_controller.rb
+++ app/controllers/section_controller.rb
@@ -11,21 +11,31 @@ class SectionController < HelperController
     end
     post '/sections/new' do
 #binding.pry
-          if   params[:name]=="" || params[:location]==""
+          if   params[:section][:name]=="" || params[:section][:location]==""
                                   flash[:message] = "Please do not leave the input sections empty when submiting an edit."
                                   redirect "/libraries/#{session[:library_id]}/sections/new"
-          elsif  !!Section.find_by(name: params[:name])
+          elsif  !!Section.find_by(name: params[:section][:name])
 
                                 flash[:message] = "This section name already exists."
                                 redirect "/libraries/#{session[:library_id]}/sections/new"
           else
 
-                @section = Section.create(name: params[:name], location: params[:location], library_id: session[:library_id])
+                @section = Section.create(params[:section])
+                @section.library_id = session[:library_id]
                 redirect "/libraries/#{session[:library_id]}/sections/#{@section.id}"
           end
     end
-    patch '' do
-
+    patch '/sections/:id' do
+#binding.pry
+        @section = Section.find_by(id: params[:id])
+        if params[:section][:name]=="" || params[:section][:location]==""
+              flash[:message] = "Please do not leave input fields empty."
+              redirect "/libraries/#{session[:library_id]}/sections/#{@section.id}/edit"
+        else
+            @section.update(params[:section])
+            flash[:message] = "Successfully updated section profile."
+            redirect "/libraries/#{session[:library_id]}/sections/#{@section.id}"
+        end
     end
     delete '/sections/:id' do
 #binding.pry
