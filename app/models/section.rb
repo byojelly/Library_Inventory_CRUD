0eb35e0a61635fd31c1f5bc0a505d6b3743b97f9
Alex <github email address>
2018-02-11 21:06:28 -0500

    section model has comments providing validation cheat sheet

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,72 @@ class Section < ActiveRecord::Base
   belongs_to :library
   has_many :books
 
+#testing validations
+#make sure exists/filled in
+  validates :name, presence: true
+  #validates :location, presence: true
+  #validates :library_id, presence: true
+
+#used for associations (has many) but should only be done on 1 side not both
+
+  #validates_associated :books
+
+#uniqueness
+  validates :name, uniqueness: { case_sensitive: false }
+
+#acceptance method for things like terms of service  you can define a message
+# =>   validates :terms_of_service, acceptance: { message: 'must be abided' }
+
+# =>    validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+
+#length
+#class Person < ApplicationRecord
+#  validates :name, length: { minimum: 2 }
+#  validates :bio, length: { maximum: 500 }
+#  validates :password, length: { in: 6..20 }
+#  validates :registration_number, length: { is: 6 }
+#end
+
+#numerical
+#class Player < ApplicationRecord
+#  validates :points, numericality: true
+#  validates :games_played, numericality: { only_integer: true }
+#end
+
+#presence
+#class Person < ApplicationRecord
+#  validates :name, :login, :email, presence: true
+#end
+
+#  validations with if statements
+#class Order < ApplicationRecord
+#  validates :card_number, presence: true, if: :paid_with_card?
+#
+#  def paid_with_card?
+#    payment_type == "card"
+#  end
+#end
+
+
+#if with multiple attributes
+#class User < ApplicationRecord
+#  with_options if: :is_admin? do |admin|
+#    admin.validates :password, length: { minimum: 10 }
+#    admin.validates :email, presence: true
+#  end
+#end
+
+# => views page
+# =>            <% if @article.errors.any? %>
+# =>              <div id="error_explanation">
+# =>                <h2><%= pluralize(@article.errors.count, "error") %> prohibited this article from being saved:</h2>
+# =>
+# =>                <ul>
+# =>                <% @article.errors.full_messages.each do |msg| %>
+# =>                  <li><%= msg %></li>
+# =>                <% end %>
+# =>                </ul>
+# =>              </div>
+# =>            <% end %>
+
 end
